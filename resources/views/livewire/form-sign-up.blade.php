251131bc72bfef19d925cf45ee6f3b331bae169e
SebastianXXI05 <kaynandredblue@gmail.com>
2023-12-01 20:53:32 -0500

    add loading in sign up form

--- resources/views/livewire/form-sign-up.blade.php
+++ resources/views/livewire/form-sign-up.blade.php
@@ -1,5 +1,5 @@
 <div>
-  <form wire:submit="signUp" class="border border-gray-700 mt-10 py-6 px-2 rounded shadow-md">
+  <form wire:submit="signUp" class="border border-gray-700 mt-10 py-6 px-2 rounded shadow-md relative">
     <label class="font-medium text-gray-900">Name</label>
     @error('name')
       <span class="text-red-600 block text-xs font-semibold">{{ $message }}</span>
@@ -29,5 +29,15 @@
     <button class="py-1 px-2 bg-blue-600 border-2 border-blue-600 rounded text-white hover:bg-transparent hover:text-black transition-all transition-300">
       Sign Up
     </button>
+
+    <div wire:loading class="w-full bg-gray-700/90 absolute top-0 left-0 h-full"> 
+      <div class="grid place-content-center h-full">
+        <div class="flex gap-6">
+          <div class="w-2 h-2 animate-pulse bg-white rounded-full"></div>
+          <div class="w-2 h-2 animate-pulse bg-white rounded-full"></div>
+          <div class="w-2 h-2 animate-pulse bg-white rounded-full"></div>
+        </div>
+      </div>
+    </div>
   </form>
 </div>
